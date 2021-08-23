ff678835e40e4a6e1e5cfdd1aa6040f3b801e96d
Tandy22 <31441581+Tandy22@users.noreply.github.com>
2021-08-23 21:09:47 +0200

    First commit
    Added combined code of antes background video with mine code. 
    Code for background video is at the top of html file and css for that html code is in Critical.css

---
+++ main.js
@@ -0,0 +1,45 @@
+let elVideos = document.getElementsByClassName('video');
+let videos = [];
+
+for (let i = 0; i < elVideos.length; i++) {
+  videos.push({
+    element: elVideos[i],
+    previous: elVideos[i - 1] || elVideos[elVideos.length - 1],
+    next: elVideos[i + 1] || elVideos[0]
+  });
+}
+
+videos.forEach(video => {
+  video.element.addEventListener('ended', () => {
+    video.next.currentTime = .5;
+    video.next.play();
+    video.next.classList.remove('hidden');
+    video.element.classList.add('hidden');
+  });
+});
+
+
+
+let elCenterContent = document.getElementsByClassName("introduction-container")[0];
+
+if (videos.length > 0) {
+  // start center content animation and remove event listener for a video
+  let startAnimation = () => {
+    elCenterContent.classList.add('introduction-container--animated');
+    videos[0].element.removeEventListener('play', startAnimation);
+  }
+
+  // REF: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play_event
+  // start center content animation when video is started (this will fix situation when video
+  // is slowly loaded, so text doesn't come out too soon)
+  videos[0].element.addEventListener('play', startAnimation);
+
+  // REF: https://stackoverflow.com/questions/10086427/what-is-the-exact-difference-between-currenttarget-property-and-target-property
+  // when animation is ended, override visibility settings and remove animation class
+  elCenterContent.addEventListener('animationend', (e) => {
+    if (e.animationName === 'fadeIn') {
+      e.target.classList.add('visible');
+      e.target.classList.remove('introduction-container--animated');
+    }
+  })
+}
